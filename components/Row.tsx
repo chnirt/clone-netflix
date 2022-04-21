29ff494d4bdf488079e5aa855bbd2e82c9106848
chnirt <trinhchinchin@gmail.com>
2022-04-22 00:03:34 +0700

    update: fix typescript

--- components/Row.tsx
+++ components/Row.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useRef } from 'react'
 import { VscChevronLeft, VscChevronRight } from 'react-icons/vsc'
 import Thumbnail from './Thumbnail'
 
-const Row = ({ title, movies }) => {
+const Row = ({ title, movies }: any) => {
   const rowRef = useRef<HTMLDivElement | null>(null)
   const handleSelect = useCallback((direction: string) => {
     console.log('hello')
@@ -24,22 +24,22 @@ const Row = ({ title, movies }) => {
       </div>
 
       <div className="group relative flex">
-        <button
+        <div
           ref={rowRef}
           className="flex w-full items-center space-x-1 overflow-y-hidden overflow-x-scroll px-12 scrollbar-hide"
         >
           {movies.length &&
-            movies.map((movie, mi) => (
+            movies.map((movie: any, mi: any) => (
               <Thumbnail key={`thumbnail-${mi}`} movie={movie} />
             ))}
-        </button>
+        </div>
 
-        <div
+        <button
           className="absolute left-0 flex h-full w-12 items-center opacity-0 transition duration-200 hover:bg-[#14141450] group-hover:opacity-100"
           onClick={handleLeft}
         >
           <VscChevronLeft className="transition hover:scale-125" size={46} />
-        </div>
+        </button>
 
         <button
           className="absolute right-0 flex h-full w-12 items-center opacity-0 transition duration-200 hover:bg-[#14141450] group-hover:opacity-100"
