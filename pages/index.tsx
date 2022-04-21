672329e5b4aefe957bc4d9614af351092572031d
chnirt <trinhchinchin@gmail.com>
2022-04-22 00:04:27 +0700

    update: fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = ({
   adventureMovies,
   animationMovies,
   crimeMovies,
-}) => {
+}: any) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
